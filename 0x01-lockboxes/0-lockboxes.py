8f1284a1673c0c2172d6b82303610cdc18726bb1
Bonziman <eloirdiwi@gmail.com>
2024-10-12 21:41:27 +0100

    added documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,21 @@
-#!/usr/bin/env python3
-"""
-This script solves the lockboxes puzzle
-"""
+#!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """
-    Determine if all boxes can be unlocked starting from box 0.
-
-    Args:
-        boxes (list of list of int): A list of boxes,
-        where each box contains a list of keys
-        that can unlock other boxes.
-
-    Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
-    """
-    unlocked = set()
-    unlocked.add(0)
-    keys = boxes[0]
-
-    while keys:
-        key = keys.pop()
-        if key not in unlocked and key < len(boxes):
-            unlocked.add(key)
-            keys.extend(boxes[key])
-
-    return len(unlocked) == len(boxes)
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
