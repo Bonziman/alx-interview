de2320a957ce4ecb680bd3b3b333c152fc780fdc
Bonziman <eloirdiwi@gmail.com>
2024-11-14 22:13:48 +0100

    initial commit

---
+++ 0x01-lockboxes/0-lockboxes2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This script solves the lockboxes puzzle
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be unlocked starting from box 0.
+
+    Args:
+        boxes (list of list of int): A list of boxes,
+        where each box contains a list of keys
+        that can unlock other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    unlocked = set()
+    unlocked.add(0)
+    keys = boxes[0]
+
+    while keys:
+        key = keys.pop()
+        if key not in unlocked and key < len(boxes):
+            unlocked.add(key)
+            keys.extend(boxes[key])
+
+    return len(unlocked) == len(boxes)
